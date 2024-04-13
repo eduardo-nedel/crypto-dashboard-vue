4e01b3cb69b6dd3ad401f3b0ead1478c97c03549
Eduardo Mueller Nedel <eduardo.nedell@gmail.com>
2024-04-13 17:45:37 -0300

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<script setup>
+import { RouterView } from 'vue-router'
+import HelloWorld from './components/HelloWorld.vue'
+</script>
+
+<template>
+  <header>
+    <div>
+      <HelloWorld msg="You did it!" />
+
+    </div>
+  </header>
+
+  <main>
+    <RouterView />
+  </main>
+</template>
